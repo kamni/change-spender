b49ae5b31478dcad95ad8729823228c48def3fb0
J Leadbetter <jleadbet@gmail.com>
2015-12-03 12:55:31 -0500

    [home mock] added calculator app branch: home-mock

--- change_spender/settings.py
+++ change_spender/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # change_spender apps
+    'calculator',
 ]
 
 MIDDLEWARE_CLASSES = [
