db3e323e57b64f992f19d46905fdfcd9fb4ff505
J Leadbetter <jleadbet@gmail.com>
2015-12-03 17:07:04 -0500

    [home mock] fiddling branch: home-mock

--- calculator/views.py
+++ calculator/views.py
@@ -6,3 +6,11 @@ class MainView(TemplateView):
 
     def get_context(self):
         return {}
+
+# Faked view. Remove later
+import simplejson as json
+from django.http import HttpResponse
+from django.views.generic import View
+class KitchenSink(View):
+    def get(self, request, *args, **kwargs):
+        context = None
