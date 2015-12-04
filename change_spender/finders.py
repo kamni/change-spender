81444e60312b16ce6ec85d77524ba6346946c479
J Leadbetter <jleadbet@gmail.com>
2015-12-04 03:50:23 -0500

    [home mock] added some docs to the finder

--- change_spender/finders.py
+++ change_spender/finders.py
@@ -2,5 +2,11 @@ from django.core.files.storage import FileSystemStorage
 from django.contrib.staticfiles.finders import BaseStorageFinder
 from django.conf import settings
 
+
 class StaticFinder(BaseStorageFinder):
+    """
+    Forces Django to use STATIC_ROOT for serving static files.
+    
+    http://stackoverflow.com/a/5964786
+    """
     storage = FileSystemStorage(settings.STATIC_ROOT, settings.STATIC_URL)
